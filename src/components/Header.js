085f8ac2ded05b1929935519559db40648ce16e7
[Abhishek L] <[labhishek8787@gmail.com]>
2024-05-24 23:46:34 +0530

    Sign in /Sign Up form && Login /heder

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+        <img
+         className='w-44'
+           src='https://pnghq.com/wp-content/uploads/netflix-logo-png-430-download-7523-1536x468.png'
+           alt=' logo'/>
+
+    </div>
+  )
+}
+
+export default Header
